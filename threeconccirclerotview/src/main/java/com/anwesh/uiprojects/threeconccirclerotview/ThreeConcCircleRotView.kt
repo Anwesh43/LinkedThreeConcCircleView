72daa92f1e5ee9a3800a4b6758d279310c4e5bf2
Anwesh43 <anweshthecool0@gmail.com>
2019-03-30 18:45:33 +0530

    adding ThreeConcCircleRot class

--- threeconccirclerotview/src/main/java/com/anwesh/uiprojects/threeconccirclerotview/ThreeConcCircleRotView.kt
+++ threeconccirclerotview/src/main/java/com/anwesh/uiprojects/threeconccirclerotview/ThreeConcCircleRotView.kt
@@ -164,7 +164,31 @@ class ThreeConcCircleRotView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class ThreeConcCircleRot(var i : Int) {
+
+        private val root : TCCRNode = TCCRNode(0)
+        private var curr : TCCRNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
