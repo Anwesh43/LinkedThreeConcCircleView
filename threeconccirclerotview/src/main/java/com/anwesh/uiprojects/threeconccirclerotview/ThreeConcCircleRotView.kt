b1596e4fa3e8dea83c10e1b3a67f02929b571f1d
Anwesh43 <anweshthecool0@gmail.com>
2019-03-30 18:33:01 +0530

    adding drawTCCRNode canvas extension method

--- threeconccirclerotview/src/main/java/com/anwesh/uiprojects/threeconccirclerotview/ThreeConcCircleRotView.kt
+++ threeconccirclerotview/src/main/java/com/anwesh/uiprojects/threeconccirclerotview/ThreeConcCircleRotView.kt
@@ -21,6 +21,7 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val parts : Int = 2
 
 fun Int.inverse() : Float = 1f / this
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
@@ -31,3 +32,29 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawTCCRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val size : Float = gap / sizeFactor
+    val rGap : Float = size / circles
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(gap * (i + 1), h / 2)
+    for (j in 0..(parts - 1)) {
+        save()
+        rotate(180f * sc2)
+        for (k in 0..(circles - 1)) {
+            val sc : Float = sc1.divideScale(k, circles)
+            val r : Float = rGap * (k + 1)
+            drawArc(RectF(-r, -r, r, r), 180f, 180f * sc, false, paint)
+        }
+        restore()
+    }
+    restore()
+}
